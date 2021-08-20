345a72710879fc6bf90a79d968ff669700d988d1
msx47 <mohitsrivastava67@gmail.com>
2021-08-20 20:57:16 +0530

    run build with ghpages

--- ghPages.js
+++ ghPages.js
@@ -1,5 +1,21 @@
 var ghpages = require("gh-pages");
+const { exec } = require("child_process");
 
-ghpages.publish("dist", function (err) {
-  console.log(err);
+exec("npm run build", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  ghpages.publish("dist", function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success");
+    }
+  });
 });
